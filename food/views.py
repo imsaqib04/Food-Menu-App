9174cec410fa07a83b6d5b52859203ce26520efb
saqib <saqibansari2011@gmail.com>
2025-08-03 13:39:55 +0530

    write view

--- food/views.py
+++ food/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Hello Saqib, This is your First Project!</h1>")
+def item(request):
+    return HttpResponse("Hello Saqib, This is a item !")
